7ecc7ca345e5382b440a0681c53ffe64d56f9941
egierl <egierl@ualberta.ca>
2019-08-17 17:58:57 -0600

    Adding PID

--- ATMega/Load_Bank_Ardunio_Code/Load_Bank_Ardunio_Code.ino
+++ ATMega/Load_Bank_Ardunio_Code/Load_Bank_Ardunio_Code.ino
@@ -3,6 +3,9 @@
                                                                                EcoCar FC Team 
                                                                                Elizabeth Gierl
                                                                                Summer 2019                                                                          */
+
+
+
 //pin mapping:
 // Pol Curve + Conditioning: Pin 16 (PB2: SS/OC1B/PCINT2)
 //Air Starve Relay: PD7 (PCINT23/AIN1)
@@ -10,13 +13,7 @@
 
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x08
-
-void setup() { 
- Wire.begin(SLAVE_ADDRESS);        // join i2c bus with address #8
- Wire.onRequest(Write_to_Pi); // register event
- Wire.onReceive(Read_From_Pi); // register event
- Serial.begin(9600);  
-}
+#include <PID_v1.h>
 
 
 //setup global varaibles 
@@ -31,12 +28,36 @@ void setup() {
   char c[]={223,8,221,40};
   char Read_Inputs[]={0,0,0,223,8,221,40};
   char output_pi[]={0,1, 1, 1}; 
-  char Temperature= 10; 
   char Fan_Speed=OCR0A;
+  char Desired_Fan_Speed=2;
   char AirStarve_Set=0;
   char Conditioning_Set=0;
   char Conditioning_Value=0;
   char Resistance=OCR1B;
+
+  
+  
+//Specify the links and initial tuning parameters
+double Setpoint, Input, Output;
+PID loadPID(&Input, &Output, &Setpoint, 2, 5, 1, DIRECT);
+// PID needs doubles is this a problem?
+
+
+double Setpoint1, Input1, Output1;
+PID fansPID(&Input1, &Output1, &Setpoint1, 2, 5, 1, DIRECT);
+// PID needs doubles is this a problem?
+
+void setup() { 
+ Wire.begin(SLAVE_ADDRESS);        // join i2c bus with address #8
+ Wire.onRequest(Write_to_Pi); // register event
+ Wire.onReceive(Read_From_Pi); // register event
+ Serial.begin(9600);  
+ //turn the PID on
+ fansPID.SetMode(AUTOMATIC);
+ loadPID.SetMode(AUTOMATIC);
+}
+
+
 
 
 
@@ -51,60 +72,44 @@ void loop() {
   char high=60;
 
 //Call functions:  
+char Read_Inputs = Read_From_Atmega();
+
+Read_From_Atmega();
 
 // Add conditions in which Airstarve vs different types of pol curve + conditioning is called
 //Conditioning : Recieve a max current from the pi code? Then 1A stepsize? to the max every one min
 //Air starve: Trigger other output ?
 //Battery Voltage? Anything but reading it?
 // Saftey Check from Jayas Code?
-
-  
+ 
   
   //Fans:
-  if (Temperature > low) {
-    Fan_Speed=0;
-   }
-  else if (low < Temperature > low_medium) {
-    Fan_Speed=5;
-   }
-  else if (low_medium < Temperature > medium) {
-    Fan_Speed==20;
-   }
-  else if (medium < Temperature > medium_high) {
-    Fan_Speed==40;
-   }
-  else if (medium_high < Temperature > high) {
-    Fan_Speed==60;
-   }
-  else if (high < Temperature) {
-    Fan_Speed== 100;
-   }
 
 
 }
-
-
 
 
 
 
 //FUNCTIONS
 
-void Read_From_Atmega() {
+char Read_From_Atmega() {
   
   char  Atmega_Status=2;//how should we collect this?
   int FC_Volt=analogRead(A1);
   int Battery_Volt=analogRead(A2);
+  
   FC_Volt_low=lowByte(FC_Volt);
   FC_Volt_high=highByte(FC_Volt);
   Battery_Volt_low=lowByte(Battery_Volt);
   Battery_Volt_high=highByte(Battery_Volt);
+  
   char CurrentCurrent=analogRead(A0);
-  char Read_Inputs[]= {0,Atmega_Status,FC_Volt_low,FC_Volt_high,Battery_Volt_low,Battery_Volt_high,CurrentCurrent}; 
-
+  char ReadInputs[]= {0,Atmega_Status,FC_Volt_low,FC_Volt_high,Battery_Volt_low,Battery_Volt_high,CurrentCurrent}; 
+  return Read_Inputs;
 }
 
-void Read_From_Pi(int howMany) {
+char Read_From_Pi(int howMany) {
   
   int i=0;
   while ( Wire.available()) { // loop through all but the last
@@ -125,7 +130,6 @@ void Read_From_Pi(int howMany) {
 void Write_to_Pi() {  
   char data[] ={0,Atmega_Status,FC_Volt_low,FC_Volt_high,Battery_Volt_low,Battery_Volt_high,CurrentCurrent}; 
   Wire.write(data); 
-  Serial.println(data); 
 }
 
 
@@ -142,31 +146,5 @@ void AirStarve(){
 }
 
 
-void Conditioning(){
-  if (Conditioning_Set==1){
-     Resistance==Conditioning_Value;
-  }
-  else{
-     Resistance==0;
-  }
-  
-}
-
-void PolCurve(){
-
-if (CurrentCurrent < DesiredCurrent){
-  Resistance = Resistance--;
-  
-}
-
-else if (CurrentCurrent > DesiredCurrent){
-  Resistance = Resistance++;
-}
-
-
-else if (CurrentCurrent == DesiredCurrent){
-  Resistance = Resistance;
-}
-
-
+void LoadControl(){
 }
