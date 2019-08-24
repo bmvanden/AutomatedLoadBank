6226295c2327896bd56a10d2b18de7993b41ba82
egierl <egierl@ualberta.ca>
2019-08-24 13:17:04 -0600

    with changes, still working good

--- ATMega/Julycode/Julycode.ino
+++ ATMega/Julycode/Julycode.ino
@@ -4,46 +4,91 @@
                                                                                Elizabeth Gierl
                                                                                Summer 2019                                                                          */
 
+//Things left to do:
+// Saftey thing
+//Signal bit 
+//make sure Air starve thing is correct..
+// double check with Jaya code 
 
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x08
+#include <PID_v1.h>
+
+// TEST THESE PID CODES   
+//Specify the links and initial tuning parameters 
+double Setpoint, Input, Output;
+PID loadPID(&Input, &Output, &Setpoint, 2, 5, 1, DIRECT);
+// PID needs doubles is this a problem?
+
+
+double Setpoint1, Input1, Output1;
+PID fansPID(&Input1, &Output1, &Setpoint1, 2, 5, 1, DIRECT);
+
 
 void setup() { 
  Wire.begin(SLAVE_ADDRESS);        // join i2c bus with address #8
  Wire.onRequest(Write_to_Pi); // register event
  Wire.onReceive(Read_From_Pi); // register event
  Serial.begin(9600);  
+  //turn the PID on
+ fansPID.SetMode(AUTOMATIC);
+ loadPID.SetMode(AUTOMATIC);
 }
 
 char c[]={223,8,221};
-
-
-//MAIN LOOP
-void loop() {
-  
 //Code for testing:
   char  Atmega_Status=2; //1 byte
   char Pi_Status= 23; // 1 byte
   int FC_Volt=46; //2 bytes
   int Battery_Volt=30; //2 bytes
   char Fan_Speed=2;//1 byte
-  char CurrentCurrent=40;//1 bytes
+  char Desired_Fan_Speed=9;
+  int CurrentCurrent=40;//1 bytes
   char DesiredCurrent=3; //1 byte
- 
-//Call functions:  
-char Read_Inputs = Read_From_Atmega();
+  char CellVoltageLow=5;
+  char CellVoltageHigh=5;
+  char BatteryVoltageLow=5;
+  char BatteryVoltageHigh=5;
+  char CellCurrentLow=5;
+  char CellCurrentHigh=5;
+  char AirStarve_Set=0;
+
+
+
+
+//MAIN LOOP
+void loop() { 
+ Read_From_Atmega(); 
+   
+  //Fans:
+double Setpoint1= 100; 
+Input1= Desired_Fan_Speed;
+Output1= Fan_Speed;
+
+
+// Load Control 
+double Setpoint= 100; 
+Input= DesiredCurrent;
+Output= CurrentCurrent;
+
 }
 
 
 //FUNCTIONS
-char Read_From_Atmega() {
+void  Read_From_Atmega() {
   
   char  Atmega_Status=2;//how should we collect this?
-  char FC_Volt=analogRead(A1);
-  char Battery_Volt=analogRead(A2);
-  char CurrentCurrent=analogRead(A0);
-  char output[]= {Atmega_Status,FC_Volt,Battery_Volt,CurrentCurrent}; // would need to convert it to char all by bit casting 
-  return output;
+  int FC_Volt=analogRead(A1);
+  int Battery_Volt=analogRead(A2);
+  int CurrentCurrent=analogRead(A0);
+   //add thisread!  Fan_Speed=;
+  
+  char CellVoltageLow=lowByte(FC_Volt);
+  char CellVoltageHigh=highByte(FC_Volt);
+  char BatteryVoltageLow=lowByte(Battery_Volt);
+  char BatteryVoltageHigh=highByte(Battery_Volt);
+  char CellCurrentLow= lowByte(CurrentCurrent);
+  char CellCurrentHigh= highByte(CurrentCurrent);
   
 }
 
@@ -55,24 +100,30 @@ while ( Wire.available()) { // loop through all but the last
     Serial.print(c[i]);
     i++;      
     }
+  Pi_Status=c[1];
+  Desired_Fan_Speed=c[2];
+  DesiredCurrent=c[3];
   
 }
   
 void Write_to_Pi() {  
 //Convert to char 
-char FC_Volt=4;
-char Battery_Volt=3;
-char Atmega_Status=4;
-char CurrentCurrent=20;
-char testvalue=5;
-  char data[] = {testvalue,testvalue, testvalue,c[1],c[2],c[3],testvalue,testvalue};
-  Wire.write(data); 
+ char testvalue=5;
+ //char data[] = {testvalue,testvalue, testvalue,c[1],c[2],c[3],testvalue,testvalue};
+ char data[] ={testvalue,Atmega_Status,CellCurrentHigh, CellCurrentLow, CellVoltageHigh,CellVoltageLow,BatteryVoltageHigh,BatteryVoltageLow};
+ Wire.write(data); 
   //Serial.println(data); 
 }
 
 
+void AirStarve(){
 
+  if(AirStarve_Set==1){
+    digitalWrite(13, HIGH);
+  }
+  
+  else{
+    digitalWrite(13, LOW);
+  }
 
-void Respond_To_States(){
-//DO LATER
 }
