6b1c90e4c3c6d6a456d9f0a20e29a92befc41714
bmvanden <44101918+bmvanden@users.noreply.github.com>
2019-08-25 18:02:49 -0600

    Update GUIThread.py

--- Pi/AppFiles/GUIThread.py
+++ Pi/AppFiles/GUIThread.py
@@ -2,7 +2,7 @@ import threading
 import tkinter as tk
 from AppFiles import I2CThread
 import time
-from AppFiles from model import EcoCarModel
+from AppFiles import model
 
 class GUIThread (threading.Thread):
     def __init__(self, threadID, name, counter):
