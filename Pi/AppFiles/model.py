57ca1fb6a9a32badceba94b88eb2af5cb2f1f732
bmvanden <44101918+bmvanden@users.noreply.github.com>
2019-08-25 18:19:10 -0600

    Update model.py

--- Pi/AppFiles/model.py
+++ Pi/AppFiles/model.py
@@ -1,6 +1,27 @@
 import configuration as cfg
 from AppFiles import I2CThread
 
+# Current operating parameters for load bank
+PiStatus = 0            # See I2CThread.py for explanation of states
+desiredEnclTemp = 35    # degrees C - fan speed adjusts to target this setpoint
+actualLoadTemp = 0      # measured above load resistors
+actualContrTemp = 0     # measured above controller board
+targetLoadCurrent = 0   # amps - desired load on fuel cell
+
+ATMegaStatus = 0
+actualLoadCurrent = 0   # amps - actual load on fuel cell
+actualLoadVoltage = 0   # volts - fuel cell voltage
+batteryVoltage = 0      # volts - 12V internal battery voltage
+
+name = none
+conditioningCurrent = none
+conditioningTime = none
+minCurrent = none
+maxCurrent = none
+currentStepSize = none
+currentStepDuration = none
+airStarveTime = none
+
 class EcoCarModel:
     def __init__(self):
         # Get vehicle specifications from configuration file
@@ -12,18 +33,6 @@ class EcoCarModel:
         currentStepSize = cfg.currentStepSize
         currentStepDuration = cfg.currentStepDuration
         airStarveTime = cfg.airStarveTime
-        
-        # Current operating parameters for load bank
-        PiStatus = 0            # See I2CThread.py for explanation of states
-        desiredEnclTemp = 35    # degrees C - fan speed adjusts to target this setpoint
-        actualLoadTemp = 0      # measured above load resistors
-        actualContrTemp = 0     # measured above controller board
-        targetLoadCurrent = 0   # amps - desired load on fuel cell
-        
-        ATMegaStatus = 0
-        actualLoadCurrent = 0   # amps - actual load on fuel cell
-        actualLoadVoltage = 0   # volts - fuel cell voltage
-        batteryVoltage = 0      # volts - 12V internal battery voltage
         
     def refreshI2CData(target):
         if I2CThread.readErrCounter == 0:
